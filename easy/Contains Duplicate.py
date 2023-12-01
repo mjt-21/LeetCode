a7551b97eee49b049b4a58ddc2312e1e826f0572
Matthew Toutjian <65698531+mjt-21@users.noreply.github.com>
2023-11-30 16:50:21 -0800

    Update Contains Duplicate.py

--- easy/Contains Duplicate.py
+++ easy/Contains Duplicate.py
@@ -1,8 +1,10 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nSet = set() # set simply used for quick lookup, checking if each number was seen before
+        nSet = set() # Hash set used for quick lookup
         for n in nums:
             if n in nSet:
-                return True # if n was seen before, it means n has at least 2 instances and thus is a duplicate
+                return True # If n was seen before, it means n has at least 2 instances and thus is a duplicate
             else:
-                nSet.add(n) # if n was not seen before, add it to the set
+                nSet.add(n) # If n was not seen before, add it to the set
+
+        return False # No duplicates found above, thus no duplicates
